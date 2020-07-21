47888430131386edfa7c635e324796ed0357b85b
Max Lv <mlv@nvidia.com>
2020-07-21 08:47:55 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(minSdk)
         targetSdkVersion(29)
-        versionCode = 1030200
-        versionName = "1.3.2"
+        versionCode = 1030300
+        versionName = "1.3.3"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
