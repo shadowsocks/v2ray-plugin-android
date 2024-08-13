e63fb0a6b87beeacca91c50456e4f8f284b001a4
Mygod <contact-git@mygod.be>
2024-08-12 21:23:53 -0700

    Fix circleci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         }
     }
     sourceSets.getByName("main").jniLibs.srcDirs(files("$projectDir/build/go"))
+    ndkVersion = "27.0.12077973"
     packagingOptions.jniLibs.useLegacyPackaging = true
 }
 
